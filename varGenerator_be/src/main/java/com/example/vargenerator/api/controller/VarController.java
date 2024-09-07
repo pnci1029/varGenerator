1ffe2d2b1fea1e8c0fbcd76bb96468efbd4b364a
chhong <pnci1029@gmail.com>
2024-09-07 14:01:46 +0900

    add default api

--- varGenerator_be/src/main/java/com/example/vargenerator/api/controller/VarController.java
+++ varGenerator_be/src/main/java/com/example/vargenerator/api/controller/VarController.java
@@ -1,9 +1,19 @@
 package com.example.vargenerator.api.controller;
 
+import com.example.vargenerator.api.service.VarService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController @RequiredArgsConstructor
 @RequestMapping("/api/var")
 public class VarController {
+    private final VarService varService;
+
+    @GetMapping("/")
+    public void getMain(HttpServletRequest request) {
+        varService.getMain(request);
+    }
 }
--- varGenerator_be/src/main/java/com/example/vargenerator/api/service/VarService.java
+++ varGenerator_be/src/main/java/com/example/vargenerator/api/service/VarService.java
@@ -1,7 +1,15 @@
 package com.example.vargenerator.api.service;
 
+import com.example.vargenerator.domain.repository.VarHistoryRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service @RequiredArgsConstructor
 public class VarService {
+    private final VarHistoryRepository varHistoryRepository;
+
+    public void getMain(HttpServletRequest request) {
+
+    }
 }
