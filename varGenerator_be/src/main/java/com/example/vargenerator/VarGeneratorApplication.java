a4c5594f1b6a5fb44f3231561a907b310d17194f
chhong <pnci1029@gmail.com>
2024-09-07 13:01:31 +0900

    Initial commit

---
+++ varGenerator_be/src/main/java/com/example/vargenerator/VarGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.vargenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VarGeneratorApplication.class, args);
+    }
+
+}
