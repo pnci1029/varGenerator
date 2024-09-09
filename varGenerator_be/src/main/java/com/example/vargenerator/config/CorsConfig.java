7cca09ce77df99c6bd43d1e5116bf67e5eb319b1
chhong <pnci1029@gmail.com>
2024-09-09 18:03:20 +0900

    add config file

---
+++ varGenerator_be/src/main/java/com/example/vargenerator/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.vargenerator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 모든 경로에 대해
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Content-Disposition", "Content-Type")
+                .allowCredentials(true);
+
+    }
+}
