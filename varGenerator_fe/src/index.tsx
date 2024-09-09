2f56041f392ec792ca149c239f9731904453a379
chhong <pnci1029@gmail.com>
2024-09-09 17:34:42 +0900

    fix js file as tsx file

---
+++ varGenerator_fe/src/index.tsx
@@ -0,0 +1,28 @@
+import {createRoot} from 'react-dom/client';
+import React from 'react';
+import './index.css';
+import {Provider} from "react-redux";
+import reportWebVitals from './reportWebVitals';
+import {Main} from "./components/Main";
+import {store} from "./slice";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
